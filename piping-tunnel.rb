229ed45e92ee6ed65b2e67c3ad6fbe81559b6509
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-12 14:01:28 +0900

    Brew formula update for piping-tunnel version v0.4.2-snapshot1

--- piping-tunnel.rb
+++ piping-tunnel.rb
@@ -2,24 +2,24 @@
 class PipingTunnel < Formula
   desc "Tunnel over HTTP with Piping Server"
   homepage "https://github.com/nwtgck/go-piping-tunnel"
-  version "0.4.2"
+  version "0.4.2-snapshot1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2/piping-tunnel-0.4.2-darwin-amd64.tar.gz"
-    sha256 "bc1a71b8bdd02f414931e85be0cdf91c427a370800934a2eb360fda6e77f428b"
+    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2-snapshot1/piping-tunnel-0.4.2-snapshot1-darwin-amd64.tar.gz"
+    sha256 "c15eea201bc0b349105d0853835ca210a8258835cf37fa93856473c6298a96b3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2/piping-tunnel-0.4.2-linux-amd64.tar.gz"
-    sha256 "fe507a43f97bca99c64d416562068cbd9b3cedb2d4b99c16694c2586abff33a5"
+    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2-snapshot1/piping-tunnel-0.4.2-snapshot1-linux-amd64.tar.gz"
+    sha256 "1b388943e05cfb2d898dbe19b83c12ecd5094c43394d0ce15b17af106ee34c6a"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2/piping-tunnel-0.4.2-linux-armv6.tar.gz"
-    sha256 "a188c736dcd06aafcc4bc8e48a1a9dcad20875656fabf2e147eede09dcf4dfa5"
+    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2-snapshot1/piping-tunnel-0.4.2-snapshot1-linux-armv6.tar.gz"
+    sha256 "116ce35ce7251f5b95a22ef151adb0a79abd913784e3de71b5dd49e3103103ce"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2/piping-tunnel-0.4.2-linux-arm64.tar.gz"
-    sha256 "8ea4c01fafaa76f0ded384b8c15ce0660f5c087f33ee97aaed9f73d453f0401a"
+    url "https://github.com/nwtgck/go-piping-tunnel/releases/download/v0.4.2-snapshot1/piping-tunnel-0.4.2-snapshot1-linux-arm64.tar.gz"
+    sha256 "1906ad7f6d9a42e9a2731b30e7db9d3c13e615478decae1b78df076b04270924"
   end
 
   def install
